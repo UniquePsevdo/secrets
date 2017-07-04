f6553be5399a7de0c60dac266c1c9ae7fdaf188f
Volodymyr Sydorov <wladimir.sidorow@gmail.com>
2017-07-04 14:15:41 +0300

    +

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdTabsModule, MdDatepickerModule} from '@angular/material';
+import {Routes, RouterModule} from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {AppComponent} from './app.component';
+import {AdminComponent} from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', redirectTo: "/",},
+    {path: 'admin', component: AdminComponent}
+];
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        AdminComponent,
+        HomeComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MdTabsModule, MdDatepickerModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [],
     bootstrap: [AppComponent]
