e5ab18eb671fef42e4c646d0aa9295c80a3c2110
VolodymyrSydorov <wladimir.sidorow@gmail.com>
2017-07-12 22:45:12 +0300

    !!!

---
+++ client/src/app/admin/auth/signin/signin.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {Router, ActivatedRoute} from "@angular/router";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {environment} from '../../../../environments/environment';
+
+@Component({
+    selector: 'app-signin',
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent implements OnInit {
+
+    constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {
+        console.log(environment);
+    }
+
+    signInForm: FormGroup;
+
+    ngOnInit() {
+        this.signInForm = new FormGroup({
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'password': new FormControl(null, Validators.required)
+        });
+    }
+
+    onSubmit() {
+        console.log(this.signInForm.value);
+    }
+
+}
---
+++ client/src/app/admin/auth/signup/signup.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {Router, ActivatedRoute} from "@angular/router";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {environment} from '../../../../environments/environment';
+
+@Component({
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+    constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {
+        console.log(environment);
+    }
+
+    signUpForm: FormGroup;
+
+    ngOnInit() {
+        this.signUpForm = new FormGroup({
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'password': new FormControl(null, Validators.required)
+        });
+    }
+
+    onSubmit() {
+        this.authService.signUp(this.signUpForm.value)
+            .subscribe((response)=> {
+                    console.log(response);
+                },
+                (err)=> {
+                    console.log(err);
+                }
+            )
+
+    }
+
+}
