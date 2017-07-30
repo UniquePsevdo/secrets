f1ea0534c182b637ce6062fc53a38ebcf63622f3
VolodymyrSydorov <wladimir.sidorow@gmail.com>
2017-07-30 23:00:37 +0300

    refresh token start

--- client/src/app/admin/auth/signin/signin.component.ts
+++ client/src/app/admin/auth/signin/signin.component.ts
@@ -3,7 +3,6 @@ import {AuthService} from "../../auth/auth.service";
 import {Router, ActivatedRoute} from "@angular/router";
 import {FormGroup, FormControl, Validators} from "@angular/forms";
 import {environment} from '../../../../environments/environment';
-import * as decode from 'jwt-decode';
 
 @Component({
     selector: 'app-signin',
@@ -13,7 +12,8 @@ import * as decode from 'jwt-decode';
 export class SigninComponent implements OnInit {
 
     constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {
-        //console.log('environment : ',environment);
+        console.log('environment : ', environment);
+        //console.log(autorefresh);
     }
 
     signInForm: FormGroup;
@@ -24,21 +24,20 @@ export class SigninComponent implements OnInit {
             'password': new FormControl(null, Validators.required)
         });
     };
-            //надо создать сервис который динамически считывает в переменную id user-a из token-a
+
     onSubmit() {
         this.authService.signIn(this.signInForm.value)
-            .subscribe((data)=> {
-                    console.log('data.token : ', data.token);
-                    console.log('decode(token) : ', decode(data.token));
-                    console.log('exp : ', JSON.parse(JSON.stringify(decode(data.token))).exp);
-                    localStorage.setItem('token', JSON.stringify({token : data.token, exp : JSON.parse(JSON.stringify(decode(data.token))).exp}));
-                    this.authService.signedIn = true;
+            .subscribe((data) => {
+                    console.log(data);
+                    if (data) {
+                        console.log(0, data.token);
+                        this.authService.onAuthorize(data.token);
+                    }
                 },
-                (err)=> {
+                (err) => {
                     console.log(err);
                 }
             )
 
     };
-
 }
--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -2,28 +2,32 @@ const jwt = require('jwt-simple');
 const User = require('../models/user');
 const config = require('../config');
 
-function tokenForUser(user){
+function tokenForUser(user) {
 	const timestamp = new Date().getTime();
 	console.log(1);
-	return jwt.encode({sub: user.id, iat: timestamp, exp: Math.round(Date.now() / (1000 + 5 * 60 * 60))}, config.secret);
+	return jwt.encode({
+		sub: user.id,
+		iat: timestamp,
+		exp: Math.round(Date.now() / (1000 + 5 * 60 * 60))
+	}, config.secret);
 }
 
-exports.signup = function(req, res, next){
+exports.signup = function (req, res, next) {
 	const email = req.body.email;
 	const password = req.body.password;
 	
 	//see if a user with a given name exist
-	User.findOne({email:email}, function(err, existingUser){
-		if(err){
+	User.findOne({email: email}, function (err, existingUser) {
+		if (err) {
 			return next(err);
 		}
 		
-		if(!email || !password){
+		if (!email || !password) {
 			return res.status(422).send({error: 'You must provide email and password'});
 		}
 		
 		//if a user exist return error
-		if(existingUser){
+		if (existingUser) {
 			return res.status(422).send({error: 'Email is in use'});
 		}
 		
@@ -33,8 +37,8 @@ exports.signup = function(req, res, next){
 			password: password
 		})
 		
-		user.save(function(err){
-			if(err){
+		user.save(function (err) {
+			if (err) {
 				return next(err);
 			}
 			res.json({token: tokenForUser(user)});
@@ -43,7 +47,8 @@ exports.signup = function(req, res, next){
 	//respond user was created
 }
 
-exports.signin = function(req, res, next){
+exports.signin = function (req, res, next) {
+	console.log('req.user', req.user);
 	//give a token to user
 	res.send({token: tokenForUser(req.user)});
 }
