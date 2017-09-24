238545d6b7807c6871477aa57cbc0e579a5b1929
VolodymyrSydorov <wladimir.sidorow@gmail.com>
2017-09-24 12:05:33 +0300

    +

--- client/src/app/admin/admin-http-requests.ts
+++ client/src/app/admin/admin-http-requests.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
-import {Http, Headers, Response} from '@angular/http';
 import {environment} from '../../environments/environment';
 import {tokenNotExpired} from 'angular2-jwt';
-import {Subject, Observable} from 'rxjs';
+import {Subject} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 
 import {ErrorService} from '../errors/error.service';
